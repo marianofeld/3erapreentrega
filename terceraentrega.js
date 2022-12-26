096b1fb247db3e08ecd27d17bb78f5c2499ca6ee
marianofeld <112523084+marianofeld@users.noreply.github.com>
2022-12-26 12:39:50 -0300

    Add files via upload

--- terceraentrega.js
+++ terceraentrega.js
@@ -1,98 +1,107 @@
-let menu = [{ nombre: "hamburguesa combo A", precio: 1500, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
-{ nombre: "hamburguesa combo B", precio: 1900, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
-{ nombre: "hamburguesa veggie", precio: 1850, vegana: true, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
-{ nombre: "hamburguesa combo C", precio: 2000, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
-{ nombre: "empanada de pollo", precio: 150, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
-{ nombre: "empanada de verdura", precio: 150, vegana: true, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" }
-]
+let menu =
+  [
+    {id:0, nombre: "hamburguesa combo A", precio: 1500, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
+    {id:1, nombre: "hamburguesa combo B", precio: 1900, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
+    {id:2, nombre: "hamburguesa veggie", precio: 1850, vegana: true, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
+    {id:3, nombre: "hamburguesa combo C", precio: 2000, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
+    {id:4, nombre: "empanada de pollo", precio: 150, vegana: false, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" },
+    {id:5, nombre: "empanada de verdura", precio: 150, vegana: true, imgUrl: "https://http2.mlstatic.com/D_NQ_NP_738725-MLA44377639664_122020-O.webp" }
+  ]
 
 let div = document.getElementById("cuadroDeCompras")
+
+mostrar(menu)
+
+// todo de vegano
+let botonTodo = document.getElementById("todo")
+botonTodo.onclick = () => { mostrar(menu) }
+
 let botonVegano = document.getElementById("botonVegano")
-botonVegano.addEventListener("click", mostrarVegano)
-let vegano = menu.filter((producto) => (producto.vegana == true))
+let vegano = menu.filter((alimento) => (alimento.vegana == true))
+botonVegano.onclick = () => { mostrar(vegano) }
+botonVegano.addEventListener("mouseover", function () {
+  botonVegano.classList.add("fondo")
+})
 
-botonVegano.addEventListener("mouseover",function() {
-  botonVegano.classList.add("fondo")})
-
-// function clase() {botonVegano.className("fondo")}
-
-for (const alimento of menu) {
-  div.innerHTML +=
-    `
-    <div class="row row-cols-1 row-cols-sm-2 g-3">
-    <div class="col">
-      <div class="card">
-        <img src="${alimento.imgUrl}" class="card-img-top" alt="card-grid-image">
-        <div class="card-body">
-          <h5 class="card-title">${alimento.nombre}</h5>
-          <p class="card-text">${alimento.precio}</p>
-        </div>
-        <button type="button" id="boton" class="btn btn-primary">Añadir al pedido</button>
-      </div>
-    </div>
-    `
-}
-
-
+// todo de la carne
 
 let botonCarne = document.getElementById("botonCarne")
-let carne = menu.filter((producto) => (producto.vegana == false))
-botonCarne.addEventListener("click", mostrarCarne)
+let carne = menu.filter((alimento) => (alimento.vegana == false))
+botonCarne.onclick = () => { mostrar(carne) }
+botonCarne.addEventListener("mouseover", function () {
+  botonCarne.classList.add("fondo2")
+})
 
+let botones = document.getElementsByClassName("boton")
 
-
-// preguntar porque no puedo hacer una funcion generica para vegano y carne en el event listener
-
-
-
-
-
-
-
-
+// carrito
+let carrito = [] 
 
 
 
 
-
-
-
-function mostrarVegano() {
-  div.innerHTML = ""
-  for (let index = 0; index < vegano.length; index++) {
-    div.innerHTML +=
-      `
-    <div class="row row-cols-1 row-cols-sm-2 g-3">
-    <div class="col">
-      <div class="card">
-        <img src="${vegano[index].imgUrl}" class="card-img-top" alt="card-grid-image">
-        <div class="card-body">
-          <h5 class="card-title">${vegano[index].nombre}</h5>
-          <p class="card-text">${vegano[index].precio}</p>
-        </div>
-        <button type="button" id="boton" class="btn btn-primary">Añadir al pedido</button>
-      </div>
-    </div>
-    `
-  }
+for (const boton of botones) {
+boton.addEventListener("click", agregarAlCarrito)
 }
 
-function mostrarCarne() {
-  div.innerHTML = ""
-  for (let index = 0; index < carne.length; index++) {
-    div.innerHTML +=
-      `
-    <div class="row row-cols-1 row-cols-sm-2 g-3">
-    <div class="col">
-      <div class="card">
-        <img src="${carne[index].imgUrl}" class="card-img-top" alt="card-grid-image">
-        <div class="card-body">
-          <h5 class="card-title">${carne[index].nombre}</h5>
-          <p class="card-text">${carne[index].precio}</p>
-        </div>
-        <button type="button" id="boton" class="btn btn-primary">Añadir al pedido</button>
-      </div>
-    </div>
-    `
-  }
+
+
+
+
+function agregarAlCarrito(e) {
+ 
+  alert("hola")
+let pedidoBuscado = menu.find(alimento => alimento.id == e.target.id)
+let posicionBuscada = carrito.findIndex(alimento => alimento.id == pedidoBuscado.id)
+if (posicionBuscada != -1){
+  carrito[posicionBuscada].unidades++
+  carrito[posicionBuscada].precioTotal = carrito[posicionBuscada].unidades*carrito[posicionBuscada].precioPorUnidad
+}else{
+  console.log("estoy")
+  carrito.push({id: pedidoBuscado.id, nombre: pedidoBuscado.nombre, precioPorUnidad: pedidoBuscado.precio, unidades: 1, precioTotal: pedidoBuscado.precio })
 }
+mostrarCarrito()
+}
+
+               
+
+
+
+let cajaDeCarrito = document.getElementById("cajaDeCarrito")
+
+function mostrarCarrito() {
+  cajaDeCarrito.innerHTML = ""
+  for (let index = 0; index < carrito.length; index++) {
+    cajaDeCarrito.innerHTML +=
+    `Pediste ${carrito[index].unidades} de ${carrito[index].nombre} y el precio es ${carrito[index].precioTotal} 
+    <br>`
+
+} 
+ let total = carrito.reduce((acc, valorTotal) => acc + valorTotal.precioTotal, 0
+)
+ cajaDeCarrito.innerHTML +=`<br>Total a pagar $${total} 
+ <br>`
+}
+
+// renderizacion de productos
+
+function mostrar(alimento) {
+  div.innerHTML = " "
+for (let index = 0; index < alimento.length; index++) {
+  div.innerHTML +=
+ 
+  `
+<div class="row row-cols-1 row-cols-sm-1 g-3" id="productoDiv">
+<div class="col">
+  <div class="card">
+    <img src="${alimento[index].imgUrl}" class="card-img-top" alt="card-grid-image">
+    <div class="card-body">
+      <h5 class="card-title">${alimento[index].nombre}</h5>
+      <p class="card-text">${alimento[index].precio}</p>
+    </div>
+    <button type="button" id="${alimento[index].id}" class="btn btn-primary boton">Añadir al pedido</button>
+  </div>
+</div>
+`
+}  
+} 
